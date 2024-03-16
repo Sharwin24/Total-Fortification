ef25b0a23f40dee46050c0be59bfae1322c11bfc
Sharwin Patil <sharwin24@gmail.com>
2024-03-15 21:44:24 -0400

    Selection logic

--- Assets/Scripts/GamePlay/DeploymentManager.cs
+++ Assets/Scripts/GamePlay/DeploymentManager.cs
@@ -1,63 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DeploymentManager : MonoBehaviour {
-    private const int numTroops = 4;
-    public Image[] troopIcons = new Image[numTroops];
-    public GameObject[] troopPrefabs = new GameObject[numTroops];
+
+
+    // DeploymentManager should find out how many GameObjects with Ally tag are in the scene
+    // and display that many buttons with the corresponding icons
+    // When a button is clicked, the player should be able to open the equipment manager for that troop
+
+    public List<Image> troopIcons;
+    public List<Button> troopButtons;
+
+    private List<GameObject> allies;
 
     private readonly Dictionary<string, int> tagToTroopIndex = new Dictionary<string, int> {
-        {"TroopBtn1", 0},
-        {"TroopBtn2", 1},
-        {"TroopBtn3", 2},
-        {"TroopBtn4", 3}
+        { "TroopBtn1", 0 },
+        { "TroopBtn2", 1 },
+        { "TroopBtn3", 2 },
+        { "TroopBtn4", 3 }
     };
 
-    private int[] troopCounts = new int[numTroops];
-    public TextMeshProUGUI[] troopCountTexts = new TextMeshProUGUI[numTroops];
-    public Button[] troopButtons = new Button[numTroops];
-
     private void Awake() {
-        for (int i = 0; i < numTroops; i++) {
-            troopCounts[i] = 2;
-        }
     }
 
     // Start is called before the first frame update
     void Start() {
+        // Collect all GameObjects with Ally tag
+        allies = GameObject.FindGameObjectsWithTag("Ally").ToList();
+
         foreach (var icon in troopIcons) {
             int index = tagToTroopIndex[icon.tag];
             if (troopButtons[index] == null) troopButtons[index] = icon.GetComponent<Button>();
-            if (troopCountTexts[index] == null) troopCountTexts[index] = icon.GetComponentInChildren<TextMeshProUGUI>();
-            troopButtons[index].onClick.AddListener(() => OnTroopButtonClicked(index));
+            troopButtons[index].onClick.AddListener(() => OnTroopButtonClicked(icon));
         }
     }
 
     // Update is called once per frame
     void Update() {
-        for (int i = 0; i < numTroops; i++) {
-            troopCountTexts[i].text = troopCounts[i].ToString();
+    }
+
+    private void SelectedTroop(Image icon) {
+        foreach (var i in troopIcons) {
+            if (i == icon) {
+                // Change to green with transparency so image can be seen
+                i.color = new Color(0, 1, 0, 0.5f);
+                break;
+            }
         }
     }
 
-    public void SetTroopCounts(int troopIndex, int count) {
-        if (troopIndex < 0 || troopIndex >= numTroops) {
-            Debug.LogError("Invalid troop index: " + troopIndex);
-            return;
+    private void DeselectAll() {
+        foreach(var i in troopIcons) {
+            i.color = Color.white;
         }
-        troopCounts[troopIndex] = count;
+    }
+
+    private void OpenEquipmentManager(GameObject allyTroop) {
+        print("Open EquipmentManager for troop " + allyTroop.name);
     }
 
 
-    public void OnTroopButtonClicked(int troopIndex) {
-        print("Button " + troopIndex + " clicked");
-        if (troopCounts[troopIndex] > 0) {
-            troopCounts[troopIndex]--;
-            // Deploy troop
-            //GameObject troop = Instantiate(troopPrefabs[troopIndex]);
-        }
+    public void OnTroopButtonClicked(Image icon) {
+        DeselectAll();
+        SelectedTroop(icon);
+        int index = tagToTroopIndex[icon.tag];
+        var troop = allies[index];
+        print("Selected troop: " + troop.name); // Troop should visually appear selected somehow
+        OpenEquipmentManager(troop);
     }
 }
