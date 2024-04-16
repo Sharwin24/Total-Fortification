7fb736ecd50d459e0082c8ab7c10519a21636053
Sharwin Patil <sharwin24@gmail.com>
2024-04-16 15:48:42 -0400

    HelpIcon is now HelpButton (#58)

--- Assets/Scripts/UI/HelpPanel.cs
+++ Assets/Scripts/UI/HelpPanel.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class HelpPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class HelpPanel : MonoBehaviour {
     public GameObject helpPanel;
+    private Button helpButton;
+    private Color defaultColor;
 
     void Start() {
-        helpPanel = GameObject.FindGameObjectWithTag("HelpPanel");
+        // Button is child of this component
+        helpButton = GetComponentInChildren<Button>();
         if (helpPanel != null) helpPanel.SetActive(false);
         else Debug.LogError("HelpPanel not found");
+        if (helpButton != null) helpButton.onClick.AddListener(ToggleHelpPanel);
+        else Debug.LogError("HelpButton not found");
+        this.defaultColor = helpButton.image.color;
     }
 
-    public void OnPointerEnter(PointerEventData eventData) {
-        helpPanel.SetActive(true);
+    void ToggleHelpPanel() {
+        if (helpPanel.activeSelf) helpPanel.SetActive(false);
+        else helpPanel.SetActive(true);
+        ColorIcon();
     }
 
-    public void OnPointerExit(PointerEventData eventData) {
-        helpPanel.SetActive(false);
+    void ColorIcon() {
+        // If the help panel is active, change the color of the help button to a slight green, otherwise default color
+        if (helpPanel.activeSelf) helpButton.image.color = new Color(0.8f, 1f, 0.8f, 1f);
+        else helpButton.image.color = this.defaultColor;
     }
 }
